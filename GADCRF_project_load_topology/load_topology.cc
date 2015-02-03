3d6a7dbef202130f187e6bc1a38f47eb2fcc08ab
darkalbo <g.alboreto@gmail.com>
2015-02-03 18:31:42 +0100

    Modificato main col codice per far partire lo script sui router

--- GADCRF_project_load_topology/load_topology.cc
+++ GADCRF_project_load_topology/load_topology.cc
@@ -9,6 +9,7 @@
 
 #include "header_project.h"
 
+void InstallLSP(Topology *net,int nodes);
 
 int main(int argc, char *argv[]) {
 
@@ -22,27 +23,73 @@ int main(int argc, char *argv[]) {
 
 	Topology net(nodes);//Topology net=new Topology(nodes);
 	net.LoadTopology(xmlTopology);
-	net.PrintAdjMatrix();
-    int size;
-	int* path = find_path(net.Matrix(),nodes,0,4,70,&size);
-	net.UpdateTopology(path,size,70);
-	net.PrintAdjMatrix();
-	int* path2 = find_path(net.Matrix(),nodes,0,4,50,&size);
-	net.UpdateTopology(path2,size,50);
-	net.PrintAdjMatrix();
 
 	printf("Select mode(0=GNS3, 1=real 2=demo)\n");
 	int mode=0;
 	scanf("%i",&mode);
 	if(mode==0){
+		//Enable tap interface and test it
 		system("chmod 700 ./script/config_tap.sh");
 		system("./script/config_tap.sh");
 		printf("test\n");
 		system("ping 10.1.1.1 -c 3");
 		printf("test ended\n");
-		system("chmod 700 ./script/telnet_test.sh");
-		system("expect ./script/telnet_test.sh 10.1.1.1");
-
 	}
+
+	int choise=0;
+	while(1){
+		printf("********* MENU *********\n");
+		printf("1: Print adjacency matrix\n");
+		printf("2: Install LSP\n");
+		printf("> ");
+		scanf("%i",&choise);
+		switch(choise){
+		case 1:
+			net.PrintAdjMatrix();
+			break;
+		case 2:
+			InstallLSP(&net,nodes);
+
+		}
+	}
+
 	return 0;
 }
+
+void InstallLSP(Topology *net,int nodes){
+
+	int size;
+	int capacity;
+	int src;
+	int dst;
+
+	printf("Source node:\n> ");
+	scanf("%i",&src);
+	printf("Destination node:\n> ");
+	scanf("%i",&dst);
+	printf("Capacity:\n> ");
+	scanf("%i",&capacity);
+	int* path = find_path(net->Matrix(),nodes,src,dst,capacity,&size);
+	if(path==NULL){
+		printf("It's not possible to install an LSP\n");
+		return;
+	}
+	net->UpdateTopology(path,size,capacity);
+	//Aprire i terminali per configurare i router!
+
+	for(int i=0;i<size;i++){
+		int index=path[i];
+		for(int j=0;j<nodes;j++){
+			if(net->Matrix()[index][j].capacity!=-1){
+				char dest[100]="";
+				strcat(dest,"expect ./script/telnet_test.sh ");
+				strcat(dest,net->Matrix()[index][j].srcAddr);
+				printf("%s\n",dest);
+				system(dest);
+				break;
+			}
+		}
+	}
+}
+
+
