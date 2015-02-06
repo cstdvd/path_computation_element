7aa42ef8a99e4f19c33afdf724cd1359953bdab1
darkalbo <g.alboreto@gmail.com>
2015-02-06 12:26:16 +0100

    Modificato script cef (che ora configura tutta la rete) Varie modifiche al main

--- GADCRF_project_load_topology/load_topology.cc
+++ GADCRF_project_load_topology/load_topology.cc
@@ -8,14 +8,43 @@
  */
 
 #include "header_project.h"
+#define INT_DIGITS 19
 
 void installLSP(Topology *net,int nodes);
 void installLSPdemo(Topology *net,int nodes);
+void configureNet(Topology *net,int nodes);
+
+int id=0;
+
+//utility, conversion from int to string
+char *itoa(int i)
+{
+  /* Room for INT_DIGITS digits, - and '\0' */
+  static char buf[INT_DIGITS + 2];
+  char *p = buf + INT_DIGITS + 1;	/* points to terminating '\0' */
+  if (i >= 0) {
+    do {
+      *--p = '0' + (i % 10);
+      i /= 10;
+    } while (i != 0);
+    return p;
+  }
+  else {			/* i < 0 */
+    do {
+      *--p = '0' - (i % 10);
+      i /= 10;
+    } while (i != 0);
+    *--p = '-';
+  }
+  return p;
+}
+
 
 int main(int argc, char *argv[]) {
 
 	struct xmlRoot2 *xmlTopology;
 	int nodes = 0;
+	char *command;
 
 	xmlTopology = (struct xmlRoot2*) malloc(sizeof(struct xmlRoot2));
 
@@ -26,20 +55,27 @@ int main(int argc, char *argv[]) {
 	net.LoadTopology(xmlTopology);
 
 	int mode;
-	while(mode!=0 && mode!=2){
+	while(mode!=0 && mode!= 1 && mode!=2){
 		printf("Select mode(0=GNS3, 1=real, 2=demo)\n> ");
 		scanf("%i",&mode);
 		switch(mode){
 		case 0:
 			//Enable tap interface and test it
-			system("chmod 700 ./script/config_tap.sh");
+			printf("Enable tap interface and test it\n");
+			//system("chmod 700 ./script/config_tap.sh");
 			system("./script/config_tap.sh");
 			printf("test\n");
-			system("ping 10.1.1.1 -c 3");
+			for (int i=0;i<nodes;i++){
+					command = (char*)calloc(CHAR_COMMAND,sizeof(char));
+					strcpy(command,"ping ");
+					strcat(command,net.LoopArray()[i].loopAddr);
+					strcat(command," -c 3");
+					system(command);
+			}
 			printf("test ended\n");
 			break;
 		case 1:
-			printf("Real mode not implemented.\n");
+			printf("Real mode\n");
 			break;
 		case 2:
 			printf("Demo mode\n");
@@ -54,8 +90,9 @@ int main(int argc, char *argv[]) {
 	while(1){
 		printf("********* MENU *********\n");
 		printf("1: Print adjacency matrix\n");
-		printf("2: Install LSP\n");
-		printf("3: Exit\n");
+		printf("2: Configure net\n");
+		printf("3: Install LSP\n");
+		printf("4: Exit\n");
 		printf("> ");
 		scanf("%i",&choise);
 		switch(choise){
@@ -63,12 +100,15 @@ int main(int argc, char *argv[]) {
 			net.PrintAdjMatrix();
 			break;
 		case 2:
-			if(mode==0)
+			configureNet(&net,nodes);
+			break;
+		case 3:
+			if(mode==0 || mode==1)
 				installLSP(&net,nodes);
 			else if(mode==2)
 				installLSPdemo(&net,nodes);
 			break;
-		case 3:
+		case 4:
 			return 0;
 		default:
 			printf("Command not found\n");
@@ -85,8 +125,7 @@ void installLSP(Topology *net,int nodes){
 	int capacity;
 	int src;
 	int dst;
-	int i,j;
-	char *command[nodes];
+	char *command;
 
 	printf("Source node:\n> ");
 	scanf("%i",&src);
@@ -100,28 +139,24 @@ void installLSP(Topology *net,int nodes){
 		return;
 	}
 	net->UpdateTopology(path,size,capacity);
-	//Aprire i terminali per configurare i router!
-
-
-
-	// per prova
-
-	for (i=0;i<nodes;i++){
-
-		command[i] = (char*)calloc(CHAR_COMMAND,sizeof(char));
-		strcpy(command[i],"expect ./script/cef.sh ");
-		strcat(command[i],net->LoopArray()[i].loopAddr);
-
-		for(j=0;j<nodes;j++){
-
-			if(net->Matrix()[i][j].capacity!=-1){
-				strcat(command[i]," ");
-				strcat(command[i],net->Matrix()[i][j].srcInterface);
-			}
-		}
-		printf("system[%i]=%s\n",i,command[i]);
-		system(command[i]);
+	command = (char*)calloc(CHAR_COMMAND,sizeof(char));
+	strcpy(command,"expect ./script/lsp.sh ");
+	strcat(command,net->LoopArray()[path[0]].loopAddr);//inserisco IP
+	strcat(command," ");
+	strcat(command,net->LoopArray()[path[size-1]].loopAddr);//inserisco DEST
+	strcat(command," ");
+	char *buffer;
+	buffer = itoa(capacity);
+	strcat(command,buffer);//Inserisco CAPACITY
+	strcat(command," ");
+	buffer = itoa(id++);
+	strcat(command,buffer);//Inserisco ID
+	strcat(command," ");
+	for(int i=0;i<size-1;i++){
+		strcat(command,net->Matrix()[path[i]][path[i+1]].dstAddr);//inserisco PATH
+		strcat(command," ");
 	}
+	system(command);
 }
 
 void installLSPdemo(Topology *net,int nodes){
@@ -143,4 +178,24 @@ void installLSPdemo(Topology *net,int nodes){
 	}
 	net->UpdateTopology(path,size,capacity);
 
+}
+
+void configureNet(Topology *net,int nodes){
+	char *command[nodes];
+	int i,j;
+	for (i=0;i<nodes;i++){
+		command[i] = (char*)calloc(CHAR_COMMAND,sizeof(char));
+		strcpy(command[i],"expect ./script/cef.sh ");
+		strcat(command[i],net->LoopArray()[i].loopAddr);
+
+		for(j=0;j<nodes;j++){
+
+			if(net->Matrix()[i][j].capacity!=-1){
+				strcat(command[i]," ");
+				strcat(command[i],net->Matrix()[i][j].srcInterface);
+			}
+		}
+		//printf("system[%i]=%s\n",i,command[i]);
+		system(command[i]);
+	}
 }
